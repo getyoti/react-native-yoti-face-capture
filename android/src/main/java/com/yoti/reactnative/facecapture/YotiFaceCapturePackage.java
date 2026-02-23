de7c9509be568f49ac230fa685e9290b3f883950
Maria Senosiain <maria.senosiain@yoti.com>
2026-02-23 16:20:46 +0000

    Remove use of `List.of`: it can cause a crash in API <= 24
    Related with YM-37765

--- android/src/main/java/com/yoti/reactnative/facecapture/YotiFaceCapturePackage.java
+++ android/src/main/java/com/yoti/reactnative/facecapture/YotiFaceCapturePackage.java
@@ -6,18 +6,19 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class YotiFaceCapturePackage implements ReactPackage {
     @NonNull
     @Override
     public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
-      return List.of(new YotiFaceCaptureModule(reactContext));
+      return Arrays.<NativeModule>asList(new YotiFaceCaptureModule(reactContext));
     }
 
     @NonNull
     @Override
     public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
-        return List.of(new YotiFaceCaptureViewManager());
+        return Arrays.<ViewManager>asList(new YotiFaceCaptureViewManager());
     }
 }
